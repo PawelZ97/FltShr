21ee4ba0d4ae58ab3840711675d1f4de807ac35e
PAwelZ97 <pawelzych97@gmail.com>
2019-11-07 18:58:15 +0100

    modules creation

---
+++ backend-fltshr/src/main/java/com/zychp/backendfltshr/BackendFltshrApplication.java
@@ -0,0 +1,13 @@
+package com.zychp.backendfltshr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFltshrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFltshrApplication.class, args);
+    }
+
+}
---
+++ backend-fltshr/src/test/java/com/zychp/backendfltshr/BackendFltshrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zychp.backendfltshr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFltshrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
