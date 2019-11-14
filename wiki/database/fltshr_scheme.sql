e419e5ab8929ac523530a5200e64354d84aec970
PAwelZ97 <pawelzych97@gmail.com>
2019-11-14 19:38:41 +0100

    database schema update

--- wiki/database/fltshr_scheme.sql
+++ wiki/database/fltshr_scheme.sql
@@ -1,90 +1,95 @@
 CREATE TABLE "advertisements" (
-                                  "id" int PRIMARY KEY NOT NULL,
+                                  "id" bigint PRIMARY KEY NOT NULL,
                                   "content" varchar(200) NOT NULL,
-                                  "creator" int NOT NULL,
+                                  "creator" bigint NOT NULL,
                                   "creation_date" timestamp NOT NULL,
                                   "expiration_date" timestamp NOT NULL
 );
 
 CREATE TABLE "advertisements_board" (
-                                        "id" int PRIMARY KEY NOT NULL,
-                                        "user" int NOT NULL,
-                                        "advertisement" int NOT NULL
+                                        "id" bigint PRIMARY KEY NOT NULL,
+                                        "user" bigint NOT NULL,
+                                        "advertisement" bigint NOT NULL
 );
 
 CREATE TABLE "bills" (
-                         "id" int PRIMARY KEY NOT NULL,
+                         "id" bigint PRIMARY KEY NOT NULL,
                          "name" varchar(50) NOT NULL,
-                         "amount" numeric(8,2),
-                         "frequency_days" int NOT NULL,
+                         "amount" bigint,
+                         "frequency_days" bigint NOT NULL,
                          "start_date" timestamp NOT NULL,
-                         "created_by" int NOT NULL,
+                         "created_by" bigint NOT NULL,
                          "created_date" timestamp NOT NULL
 );
 
 CREATE TABLE "chores_assigned" (
-                                   "id" int PRIMARY KEY NOT NULL,
-                                   "user" int NOT NULL,
-                                   "chore" int NOT NULL,
+                                   "id" bigint PRIMARY KEY NOT NULL,
+                                   "user" bigint NOT NULL,
+                                   "chore" bigint NOT NULL,
                                    "assign_date" timestamp NOT NULL,
                                    "done" boolean NOT NULL,
                                    "done_date" timestamp NOT NULL
 );
 
-CREATE TABLE "expense_usages" (
-                                  "id" int PRIMARY KEY NOT NULL,
-                                  "used_by" int NOT NULL,
-                                  "expense" int NOT NULL,
-                                  "type" varchar(10) NOT NULL,
-                                  "percent" int,
-                                  "units" int
+CREATE TABLE "expense_unequal" (
+                                   "id" bigint PRIMARY KEY NOT NULL,
+                                   "used_by" bigint NOT NULL,
+                                   "expense" bigint NOT NULL,
+                                   "percent" bigint,
+                                   "units" bigint
 );
 
 CREATE TABLE "expenses" (
-                            "id" int PRIMARY KEY NOT NULL,
+                            "id" bigint PRIMARY KEY NOT NULL,
                             "name" varchar(50) NOT NULL,
-                            "total" numeric(8,2) NOT NULL,
-                            "paid_by" int NOT NULL,
-                            "bought_date" timestamp NOT NULL,
+                            "total" bigint NOT NULL,
+                            "is_equal" boolean NOT NULL,
+                            "list" bigint NOT NULL,
+                            "paid_by" bigint NOT NULL,
+                            "timestamp" timestamp NOT NULL,
                             "description" varchar(100)
 );
 
+CREATE TABLE "expenses_lists" (
+                                  "id" bigint PRIMARY KEY NOT NULL,
+                                  "name" varchar(50) NOT NULL
+);
+
 CREATE TABLE "frequent_chores" (
-                                   "id" int PRIMARY KEY NOT NULL,
+                                   "id" bigint PRIMARY KEY NOT NULL,
                                    "description" varchar(100) NOT NULL,
-                                   "duration_days" int NOT NULL,
-                                   "frequency_days" int NOT NULL
+                                   "duration_days" bigint NOT NULL,
+                                   "frequency_days" bigint NOT NULL
 );
 
 CREATE TABLE "queue_chores" (
-                                "id" int PRIMARY KEY NOT NULL,
+                                "id" bigint PRIMARY KEY NOT NULL,
                                 "description" varchar(100) NOT NULL
 );
 
-CREATE TABLE "shopping_data" (
-                                 "id" int PRIMARY KEY NOT NULL,
-                                 "shopping_list" int NOT NULL,
-                                 "item" int NOT NULL,
-                                 "bought" boolean NOT NULL,
-                                 "bought_date" timestamp NOT NULL,
-                                 "buyer" int NOT NULL
+CREATE TABLE "shopping_entries" (
+                                    "id" bigint PRIMARY KEY NOT NULL,
+                                    "shopping_list" bigint NOT NULL,
+                                    "item" bigint NOT NULL,
+                                    "bought_by" boolean NOT NULL,
+                                    "bought_date" timestamp NOT NULL,
+                                    "buyer" bigint NOT NULL
 );
 
 CREATE TABLE "shopping_items" (
-                                  "id" int PRIMARY KEY NOT NULL,
+                                  "id" bigint PRIMARY KEY NOT NULL,
                                   "description" varchar(50) NOT NULL
 );
 
 CREATE TABLE "shopping_lists" (
-                                  "id" int PRIMARY KEY NOT NULL,
-                                  "description" varchar(100) NOT NULL,
-                                  "shopping_data_id" int NOT NULL
+                                  "id" bigint PRIMARY KEY NOT NULL,
+                                  "desription" varchar(100) NOT NULL
 );
 
 CREATE TABLE "users" (
-                         "id" int PRIMARY KEY NOT NULL,
+                         "id" bigint PRIMARY KEY NOT NULL,
                          "username" varchar(20) NOT NULL,
-                         "password" varchar(100) NOT NULL,
+                         "password" varchar(20) NOT NULL,
                          "email" varchar(30) NOT NULL,
                          "role" varchar(10) NOT NULL,
                          "registration_date" timestamp NOT NULL
@@ -100,18 +105,20 @@ ALTER TABLE "bills" ADD FOREIGN KEY ("created_by") REFERENCES "users" ("id");
 
 ALTER TABLE "chores_assigned" ADD FOREIGN KEY ("chore") REFERENCES "frequent_chores" ("id");
 
-ALTER TABLE "expense_usages" ADD FOREIGN KEY ("expense") REFERENCES "expenses" ("id");
+ALTER TABLE "expense_unequal" ADD FOREIGN KEY ("expense") REFERENCES "expenses" ("id");
 
-ALTER TABLE "expense_usages" ADD FOREIGN KEY ("used_by") REFERENCES "users" ("id");
+ALTER TABLE "expense_unequal" ADD FOREIGN KEY ("used_by") REFERENCES "users" ("id");
 
 ALTER TABLE "expenses" ADD FOREIGN KEY ("paid_by") REFERENCES "users" ("id");
 
 ALTER TABLE "chores_assigned" ADD FOREIGN KEY ("chore") REFERENCES "queue_chores" ("id");
 
-ALTER TABLE "shopping_data" ADD FOREIGN KEY ("item") REFERENCES "shopping_items" ("id");
+ALTER TABLE "shopping_entries" ADD FOREIGN KEY ("item") REFERENCES "shopping_items" ("id");
 
-ALTER TABLE "shopping_data" ADD FOREIGN KEY ("shopping_list") REFERENCES "shopping_lists" ("id");
+ALTER TABLE "shopping_entries" ADD FOREIGN KEY ("shopping_list") REFERENCES "shopping_lists" ("id");
 
 ALTER TABLE "chores_assigned" ADD FOREIGN KEY ("user") REFERENCES "users" ("id");
 
-ALTER TABLE "shopping_data" ADD FOREIGN KEY ("buyer") REFERENCES "users" ("id");
+ALTER TABLE "shopping_entries" ADD FOREIGN KEY ("buyer") REFERENCES "users" ("id");
+
+ALTER TABLE "expenses" ADD FOREIGN KEY ("list") REFERENCES "expenses_lists" ("id");
