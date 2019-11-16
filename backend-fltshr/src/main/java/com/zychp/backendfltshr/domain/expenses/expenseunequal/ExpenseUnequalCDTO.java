543791d3dcd284a80c41a3864b18a3cf06976276
PAwelZ97 <pawelzych97@gmail.com>
2019-11-16 10:51:06 +0100

    fixed retarded mapping JSON -> CDTO error (no @Data)

--- backend-fltshr/src/main/java/com/zychp/backendfltshr/domain/expenses/expenseunequal/ExpenseUnequalCDTO.java
+++ backend-fltshr/src/main/java/com/zychp/backendfltshr/domain/expenses/expenseunequal/ExpenseUnequalCDTO.java
@@ -1,7 +1,9 @@
 package com.zychp.backendfltshr.domain.expenses.expenseunequal;
 
 import com.zychp.backendfltshr.domain.user.dto.UserNameDTO;
+import lombok.Data;
 
+@Data
 public class ExpenseUnequalCDTO {
     private UserNameDTO usedBy;
     private Long units;
@@ -9,7 +11,9 @@ public class ExpenseUnequalCDTO {
 
     public static ExpenseUnequal valueOf(ExpenseUnequalCDTO dto) {
         ExpenseUnequal entity = new ExpenseUnequal();
-        entity.setUsedBy(UserNameDTO.valueOf(dto.usedBy));
+        entity.setUsedBy(UserNameDTO.valueOf(dto.getUsedBy()));
+        entity.setUnits(dto.getUnits());
+        entity.setPercent(dto.getPercent());
         return entity;
     }
 }
