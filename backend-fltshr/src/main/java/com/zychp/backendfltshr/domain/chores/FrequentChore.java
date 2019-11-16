97eb24dd337c1828c58feb8aebd02f584695583e
PAwelZ97 <pawelzych97@gmail.com>
2019-11-16 12:51:40 +0100

    chores entities

---
+++ backend-fltshr/src/main/java/com/zychp/backendfltshr/domain/chores/FrequentChore.java
@@ -0,0 +1,31 @@
+package com.zychp.backendfltshr.domain.chores;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "frequent_chores")
+public class FrequentChore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "duration_days")
+    private Long durationDays;
+
+    @Column(name = "frequency_days")
+    private Long frequencyDays;
+}
---
+++ backend-fltshr/src/main/java/com/zychp/backendfltshr/domain/chores/FrequentChoreAssigned.java
@@ -0,0 +1,40 @@
+package com.zychp.backendfltshr.domain.chores;
+
+import com.zychp.backendfltshr.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "queue_chores_assigned")
+public class FrequentChoreAssigned {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "chore", nullable = false)
+    private QueueChore queueChore;
+
+    @CreationTimestamp
+    @Column(name = "assign_date")
+    private Timestamp assignDate;
+
+    @Column(name = "done")
+    private Boolean done;
+
+    @Column(name = "done_date")
+    private Timestamp doneDate;
+}
