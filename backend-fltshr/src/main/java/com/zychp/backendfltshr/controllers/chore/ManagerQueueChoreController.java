71fe903ec3d4881f35c28b39d65fd2422b0da593
PAwelZ97 <pawelzych97@gmail.com>
2019-11-21 15:52:14 +0100

    QueueChore logic moved to service

--- backend-fltshr/src/main/java/com/zychp/backendfltshr/controllers/chore/ManagerQueueChoreController.java
+++ backend-fltshr/src/main/java/com/zychp/backendfltshr/controllers/chore/ManagerQueueChoreController.java
@@ -1,53 +1,32 @@
 package com.zychp.backendfltshr.controllers.chore;
 
-import com.zychp.backendfltshr.repos.chore.AssignedQueueChoreRepository;
-import com.zychp.backendfltshr.repos.chore.QueueChoreRepository;
-import com.zychp.backendfltshr.model.chore.queuechores.*;
-import com.zychp.backendfltshr.model.user.User;
-import com.zychp.backendfltshr.repos.UserRepository;
+import com.zychp.backendfltshr.model.chore.queuechores.QueueChoreCDTO;
+import com.zychp.backendfltshr.model.chore.queuechores.QueueChoreDTO;
+import com.zychp.backendfltshr.services.QueueChoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "manager/chores")
 @RequiredArgsConstructor
 public class ManagerQueueChoreController {
-    private final QueueChoreRepository queueChoreRepository;
-    private final AssignedQueueChoreRepository assignedQueueChoreRepository;
-    private final UserRepository userRepository;
+    private final QueueChoreService queueChoreService;
 
     @GetMapping("/queuechores")
     ResponseEntity getQueueChores() {
-        List<QueueChore> found = (List<QueueChore>) queueChoreRepository.findAll();
-        return ResponseEntity.ok(found.stream().map(QueueChoreDTO::valueOf).collect(Collectors.toList()));
+        return ResponseEntity.ok(queueChoreService.getQueueChores());
     }
 
     @PostMapping("/queuechore/user/{userId}")
     ResponseEntity<QueueChoreDTO> createQueueChore(@PathVariable Long userId,
                                                    @RequestBody QueueChoreCDTO queueChoreCDTO) {
-        QueueChore received = QueueChoreCDTO.valueOf(queueChoreCDTO);
-        QueueChore created = queueChoreRepository.save(received);
-
-        User user = userRepository.findById(userId).orElseThrow();
-
-        AssignedQueueChore firstCreated = new AssignedQueueChore();
-        firstCreated.setQueueChore(created);
-        firstCreated.setDone(false);
-        firstCreated.setAssignedUser(user);
-        assignedQueueChoreRepository.save(firstCreated);
-
-        return ResponseEntity.accepted().body(QueueChoreDTO.valueOf(created));
-
+        return ResponseEntity.accepted().body(queueChoreService.createQueueChore(userId, queueChoreCDTO));
     }
 
     @DeleteMapping("/queuechore/{queueChoreId}")
     ResponseEntity deleteQueueChore(@PathVariable Long queueChoreId) {
-        //TODO Fix delete when queueChore assigned (relation inversion)
-        queueChoreRepository.deleteById(queueChoreId);
+        queueChoreService.deleteQueueChore(queueChoreId);
         return ResponseEntity.accepted().build();
     }
 }
---
+++ backend-fltshr/src/main/java/com/zychp/backendfltshr/services/QueueChoreService.java
@@ -0,0 +1,91 @@
+package com.zychp.backendfltshr.services;
+
+import com.zychp.backendfltshr.model.chore.queuechores.*;
+import com.zychp.backendfltshr.model.user.User;
+import com.zychp.backendfltshr.repos.UserRepository;
+import com.zychp.backendfltshr.repos.chore.AssignedQueueChoreRepository;
+import com.zychp.backendfltshr.repos.chore.QueueChoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class QueueChoreService {
+    private final AssignedQueueChoreRepository assignedQueueChoreRepository;
+    private final QueueChoreRepository queueChoreRepository;
+    private final UserRepository userRepository;
+
+    public List<AssignedQueueChoreDTO> getAssignedQueueChores() {
+        List<AssignedQueueChore> assignedQueueChores = (List<AssignedQueueChore>) assignedQueueChoreRepository.findAll();
+        System.out.println("assignedQueueChores = " + assignedQueueChores);
+        return assignedQueueChores.stream().map(AssignedQueueChoreDTO::valueOf).collect(Collectors.toList());
+    }
+
+    public List<AssignedQueueChoreDTO> getAssignedQueueChoresMe() {
+        String requestUsername = SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal().toString();
+        List<AssignedQueueChore> assignedQueueChores =
+                assignedQueueChoreRepository.findByAssignedUser_UsernameAndAndDoneIsFalse(requestUsername);
+        return assignedQueueChores.stream().map(AssignedQueueChoreDTO::valueOf).collect(Collectors.toList());
+    }
+
+    public AssignedQueueChoreDTO setDone(Long queueChoreId) {
+        String requestUsername = SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal().toString();
+
+        AssignedQueueChore doneChore = assignedQueueChoreRepository.findById(queueChoreId).orElseThrow();
+        if (!doneChore.getAssignedUser().getUsername().equals(requestUsername)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Chore not yours");
+        }
+        if (doneChore.getDone()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Chore already done");
+        }
+        doneChore.setDone(true);
+        doneChore.setDoneDate(new Timestamp(System.currentTimeMillis()));
+        AssignedQueueChore responseChore = assignedQueueChoreRepository.save(doneChore);
+
+        AssignedQueueChore autoAssign = new AssignedQueueChore();
+        autoAssign.setDone(false);
+        autoAssign.setQueueChore(doneChore.getQueueChore());
+
+        Long newUserId = (doneChore.getAssignedUser().getId() - 2) % (userRepository.count() - 2) + 3;
+        autoAssign.setAssignedUser(userRepository.findById(newUserId).orElseThrow());
+
+        assignedQueueChoreRepository.save(autoAssign);
+        return AssignedQueueChoreDTO.valueOf(responseChore);
+    }
+
+
+    public List<QueueChoreDTO> getQueueChores() {
+        List<QueueChore> found = (List<QueueChore>) queueChoreRepository.findAll();
+        return found.stream().map(QueueChoreDTO::valueOf).collect(Collectors.toList());
+    }
+
+    public QueueChoreDTO createQueueChore(Long userId, QueueChoreCDTO queueChoreCDTO) {
+        QueueChore received = QueueChoreCDTO.valueOf(queueChoreCDTO);
+        QueueChore created = queueChoreRepository.save(received);
+
+        User user = userRepository.findById(userId).orElseThrow();
+
+        AssignedQueueChore firstCreated = new AssignedQueueChore();
+        firstCreated.setQueueChore(created);
+        firstCreated.setDone(false);
+        firstCreated.setAssignedUser(user);
+        assignedQueueChoreRepository.save(firstCreated);
+
+        return QueueChoreDTO.valueOf(created);
+    }
+
+
+    public void deleteQueueChore(Long queueChoreId) {
+        //TODO Fix delete when queueChore assigned (relation inversion)
+        queueChoreRepository.deleteById(queueChoreId);
+    }
+}
