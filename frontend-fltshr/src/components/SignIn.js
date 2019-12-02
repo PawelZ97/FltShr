bf55ca00b3c8ad67636ca4a007b83d6604e7423f
PAwelZ97 <pawelzych97@gmail.com>
2019-12-02 13:59:35 +0100

    Working SignIn component, deleted SignInButton

--- frontend-fltshr/src/components/SignIn.js
+++ frontend-fltshr/src/components/SignIn.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import {API_ADDRESS} from "../utils/constants";
 import AppBarView from "./AppBarView";
 import {makeStyles} from '@material-ui/core/styles';
-import SignInButton from "./SignInButton";
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -24,12 +27,39 @@ const useStyles = makeStyles(theme => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
-    signinbutton: {
+    signInButton: {
         marginTop: 20
     }
 }));
 
+
 function SignIn() {
+    let history = useHistory();
+
+    const [usernameForm, setUsernameForm] = useState("");
+    const [passwordForm, setPasswordForm] = useState("");
+
+    function callLogin() {
+        axios.post(API_ADDRESS + '/login', {
+            username: usernameForm,
+            password: passwordForm
+        })
+            .then(function (response) {
+                localStorage.setItem("authToken", response.headers.authorization);
+                console.log("Login Succesfull");
+                history.push("/dashboard");
+            })
+            .catch(function(error) {
+                if (error.response) {
+                    console.log("Status not OK");
+                } else if (error.request) {
+                    console.log("Can't connect to API");
+                } else {
+                    console.log("Something went wrong");
+                }
+            });
+    }
+
     const classes = useStyles();
     return (
         <Container component="main" maxWidth="xs">
@@ -39,6 +69,8 @@ function SignIn() {
                 </Typography>
                 <form className={classes.form} noValidate>
                     <TextField
+                        onChange={event => setUsernameForm(event.target.value)}
+                        value={usernameForm}
                         variant="outlined"
                         margin="normal"
                         required
@@ -50,6 +82,8 @@ function SignIn() {
                         autoFocus
                     />
                     <TextField
+                        onChange={event => setPasswordForm(event.target.value)}
+                        value={passwordForm}
                         variant="outlined"
                         margin="normal"
                         required
@@ -59,9 +93,8 @@ function SignIn() {
                         type="password"
                         id="password"
                     />
-                    <div className={classes.signinbutton}>
-                        <SignInButton/>
-                    </div>
+                    <Button onClick={(e) => callLogin(e)} className={classes.signInButton}
+                            fullWidth variant="contained" color="primary" size={"large"}> Zaloguj </Button>
                 </form>
             </div>
         </Container>);
