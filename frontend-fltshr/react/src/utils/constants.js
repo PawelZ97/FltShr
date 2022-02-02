86aba2947f1d57b12044517b10bbf31455d7186c
PAwel_Z97 <pawelzych97@gmail.com>
2022-02-02 01:08:58 +0100

    domain change

--- frontend-fltshr/react/src/utils/constants.js
+++ frontend-fltshr/react/src/utils/constants.js
@@ -1,2 +1,2 @@
-//export const API_ADDRESS = "http://192.168.1.117:81";
-export const API_ADDRESS = "https://fltshr.pl/api";
+//export const API_ADDRESS = "http://192.168.0.214:81";
+export const API_ADDRESS = "https://pawelzych.pl/api";
